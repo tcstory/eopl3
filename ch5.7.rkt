92115173bffdb540f85969c685f1539b066ee2b8
再见田园犬 <tcstory@163.com>
2019-07-15 17:20:29 +0800

    modify ch5.7

--- ch5.7.rkt
+++ ch5.7.rkt
@@ -16,7 +16,13 @@
     (expression ("if" expression "then" expression "else" expression) if-exp)
     (expression ("let" (arbno my-identifier "=" expression) "in" expression) let-exp)
     (expression ("-" "(" expression "," expression ")") diff-exp)
-    (expression ("(" expression expression ")") call-exp)))
+    (expression ("(" expression expression ")") call-exp)
+    (expression ("cons" "(" expression "," expression ")") cons-exp)
+    (expression ("emptylist") emptylist-exp)
+    (expression ("car" "(" expression ")") car-exp)
+    (expression ("cdr" "(" expression ")") cdr-exp)
+    (expression ("null?" "(" expression ")") null?-exp)
+    (expression ("list" "(" (separated-list expression ",") ")") list-exp)))
 
 (sllgen:make-define-datatypes the-lexical-spec the-grammar)
 
@@ -73,7 +79,7 @@
   (pair-val
    (car expval?)
    (cdr expval?))
-  (emptylist-val)
+  (emptypair-val)
   (proc-val
    (proc proc?)))
 
@@ -110,6 +116,28 @@
     (eopl:error 'expval-extractors "Looking for a ~s, found ~s"
                 variant value)))
 
+(define (get-pair-car val)
+  (cases expval val
+    (pair-val (car-val cdr-val) car-val)
+    (else (report-expval-extractor-error 'pair val))))
+
+(define (get-pair-cdr val)
+  (cases expval val
+    (pair-val (car-val cdr-val) cdr-val)
+    (else (report-expval-extractor-error 'pair val))))
+
+(define (is-null? val)
+  (cases expval val
+    (emptypair-val () (bool-val #t))
+    (else (bool-val #f))))
+
+;; 这个函数我没想到, 看了别人的答案才明白的, 我之前花了挺长时间在思考如何把 pair 给串联起来...
+(define (list->pairs vals)
+  (if (null? vals)
+      (emptypair-val)
+      (pair-val (car vals)
+                (list->pairs (cdr vals)))))
+
 (define-datatype continuation continuation?
   (end-cont)
   (zero1-cont
@@ -118,14 +146,13 @@
    (vars (list-of identifier?))
    (exps (list-of expression?))
    (body expression?)
-      (new-env environment?)
-   (old-env environment?)
-   
+   (binding-eval-env environment?)
+   (body-eval-env environment?)
    (cont continuation?))
   (if-test-cont
    (exp2 expression?)
    (exp3 expression?)
-(saved-env environment?)
+   (env environment?)
    (cont continuation?))
   (diff1-cont
    (exp2 expression?)
@@ -140,8 +167,25 @@
    (cont continuation?))
   (rand-cont
    (rator expval?)
+   (cont continuation?))
+  (pair-car-cont
+   (exp2 expression?)
+   (env environment?)
+   (cont continuation?))
+  (pair-cdr-cont
+   (car-val expval?)
+   (cont continuation?))
+  (get-pair-car-cont
+   (cont continuation?))
+  (get-pair-cdr-cont
+   (cont continuation?))
+  (null?-cont
+   (cont continuation?))
+  (list-exp-cont
+   (vals (list-of expval?))
+   (exps (list-of expression?))
+   (env environment?)
    (cont continuation?)))
-
 
 (define (apply-cont cont val)
   (cases continuation cont
@@ -151,12 +195,12 @@
                 val))
     (zero1-cont (saved-cont)
                 (apply-cont saved-cont (bool-val (zero? (expval->num val)))))
-    (let-exp-cont (vars exps body new-env old-env saved-cont)
-                  (let ((my-env (extend-env (car vars) val new-env)))
+    (let-exp-cont (vars exps body binding-eval-env body-eval-env saved-cont)
+                  (let ((my-env (extend-env (car vars) val body-eval-env)))
                     (if (= (length vars) 1)
                         (value-of/k body my-env saved-cont)
-                        (value-of/k (car exps) old-env
-                                  (let-exp-cont (cdr vars) (cdr exps) body my-env old-env saved-cont)))))
+                        (value-of/k (car exps) binding-eval-env
+                                    (let-exp-cont (cdr vars) (cdr exps) body binding-eval-env my-env saved-cont)))))
     (if-test-cont (exp2 exp3 saved-env saved-cont)
                   (if (expval->bool val)
                       (value-of/k exp2 saved-env saved-cont)
@@ -167,12 +211,34 @@
                 (let ((num1 (expval->num val1))
                       (num2 (expval->num val)))
                   (apply-cont saved-cont (num-val (- num1 num2)))))
-    (rator-cont (rand env saved-cont)
-                (value-of/k rand env
-                            (rand-cont val  saved-cont)))
-    (rand-cont (rator  saved-cont)
+    (rator-cont (rand saved-env saved-cont)
+                (value-of/k rand saved-env
+                            (rand-cont val saved-cont)))
+    (rand-cont (rator saved-cont)
                (let ((proc1 (expval->proc rator)))
-                 (apply-procedure/k proc1 val saved-cont)))))
+                 (apply-procedure/k proc1 val saved-cont)))
+    (pair-car-cont (exp2 saved-env saved-cont)
+                   (value-of/k exp2 saved-env
+                               (pair-cdr-cont val saved-cont)))
+    (pair-cdr-cont (car-val saved-cont)
+                   (apply-cont saved-cont
+                               (pair-val car-val val)))
+    (get-pair-car-cont (saved-cont)
+                       (apply-cont saved-cont (get-pair-car val)))
+    (get-pair-cdr-cont (saved-cont)
+                       (apply-cont saved-cont (get-pair-cdr val)))
+    (null?-cont (saved-cont)
+                (apply-cont saved-cont (is-null? val)))
+    (list-exp-cont (vals exps saved-env saved-cont)
+                   (let ((new-vals (append vals (list val))))
+                     (if (null? exps)
+                         (apply-cont saved-cont (list->pairs new-vals))
+                         (value-of/k (car exps) saved-env
+                                     (list-exp-cont new-vals
+                                                    (cdr exps)
+                                                    saved-env
+                                                    saved-cont)))))))
+
 
 
 (define (run string)
@@ -203,4 +269,21 @@
               (value-of/k exp1 env (diff1-cont exp2 env cont)))
     (call-exp (rator rand)
               (value-of/k rator env
-                          (rator-cont rand env cont)))))
+                          (rator-cont rand env cont)))
+    (cons-exp (exp1 exp2)
+              (value-of/k exp1 env
+                          (pair-car-cont exp2 env cont)))
+    (emptylist-exp ()
+                   (apply-cont cont (emptypair-val)))
+    (car-exp (exp1)
+             (value-of/k exp1 env
+                         (get-pair-car-cont cont)))
+    (cdr-exp (exp1)
+             (value-of/k exp1 env
+                         (get-pair-cdr-cont cont)))
+    (null?-exp (exp1)
+               (value-of/k exp1 env
+                           (null?-cont cont)))
+    (list-exp (exps)
+              (value-of/k (car exps) env
+                          (list-exp-cont (list) (cdr exps) env cont)))))
